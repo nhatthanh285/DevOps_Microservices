674cfed2cf1bfd318ece035d415eb3dc64aa8710
ThanhTN <thanhtn0528@gmail.com>
2023-11-24 11:07:37 +0700

    Commit all completed files

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=thanhtran285/ml-microservice-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=thanhtran285
+docker tag ml-microservice-api $dockerpath
+docker push $dockerpath
 
 # Step 3:
 # Push image to a docker repository
